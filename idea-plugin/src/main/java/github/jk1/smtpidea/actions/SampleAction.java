0dc275a967b65bbd6d1d9c662063f8024d41aef5
Evgeniy Naumenko <eugenbox@gmail.com>
2013-11-07 18:44:10 +0400

    Initial folder structure

---
+++ idea-plugin/src/main/java/github/jk1/smtpidea/actions/SampleAction.java
@@ -0,0 +1,19 @@
+package github.jk1.smtpidea.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+
+ */
+public class SampleAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+
+    }
+}
