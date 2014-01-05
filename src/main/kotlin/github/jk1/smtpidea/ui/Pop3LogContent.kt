077a92d098b5ee0ca0f55ef573af4e63d2726464
Jk1 <eugenbox@gmail.com>
2014-01-06 01:31:34 +0400

    Changed tree model listener type to fix the build

--- src/main/kotlin/github/jk1/smtpidea/ui/Pop3LogContent.kt
+++ src/main/kotlin/github/jk1/smtpidea/ui/Pop3LogContent.kt
@@ -2,25 +2,36 @@ package github.jk1.smtpidea.ui
 
 import github.jk1.smtpidea.log.Pop3Log
 import javax.swing.JTree
-import org.apache.log4j.lf5.viewer.categoryexplorer.TreeModelAdapter
 import com.intellij.ui.components.JBScrollPane
 import java.awt.BorderLayout
 import github.jk1.smtpidea.actions.ActionRegistry
 import javax.swing.event.TreeModelEvent
+import javax.swing.event.TreeModelListener
 
 /**
  *
  */
 class Pop3LogContent : BaseContent("POP3 Log") {
+
+    val logTree = JTree(Pop3Log);
+
+    val pop3LogListener = object : TreeModelListener{
+
+        override fun treeNodesInserted(e: TreeModelEvent?) {
+            logTree.expandPath(e?.getTreePath());
+        }
+
+        override fun treeNodesChanged(e: TreeModelEvent?) {}
+        override fun treeNodesRemoved(e: TreeModelEvent?) {}
+        override fun treeStructureChanged(e: TreeModelEvent?) {}
+    }
+
     {
-        val logTree = JTree(Pop3Log);
         logTree.setRootVisible(false);
-        Pop3Log.addTreeModelListener(object : TreeModelAdapter(){
-            override fun treeNodesInserted(e: TreeModelEvent?) {
-                logTree.expandPath(e?.getTreePath());
-            }
-        })
+        Pop3Log.addTreeModelListener(pop3LogListener)
         add(JBScrollPane(logTree), BorderLayout.CENTER);
         add(createActionsButtonPane(ActionRegistry.CLEAR_POP3_LOG), BorderLayout.WEST);
     }
+
+
 }
